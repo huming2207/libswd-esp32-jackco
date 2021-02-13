51b8537fcd76c21971a2d6b58287f3fbf11d055f
Jackson Ming Hu <huming2207@gmail.com>
2021-02-13 12:01:28 +1100

    Code cleanup

--- SWD_SPI.c
+++ SWD_SPI.c
@@ -3,6 +3,7 @@
  * ESP32 SPI driver 
  *
  * Copyright (C) 2018, Paul Freund
+ * Copyright (C) 2021, Jackson Ming Hu <huming2207@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -42,172 +43,177 @@
 #include <driver/spi_master.h>
 #include <driver/gpio.h>
 #include <libswd.h>
+#include <esp_log.h>
 
-//#define spi_printf(...) ets_printf(__VA_ARGS__)
-#define spi_printf(...) 
+#define TAG "libswd"
 
-inline void transmitSPI(spi_device_handle_t devSPI, spi_transaction_t* transSPI) {
-    if(ESP_OK != spi_device_transmit(devSPI, transSPI)) {
-        spi_printf(" - FAIL\n");
+inline void spi_transmit(spi_device_handle_t spi_dev, spi_transaction_t *spi_trans)
+{
+    if (ESP_OK != spi_device_transmit(spi_dev, spi_trans)) {
+        ESP_LOGD(TAG, " - FAIL\n");
     }
 }
 
-extern int libswd_drv_mosi_8(libswd_ctx_t *libswdctx, libswd_cmd_t *cmd, char *data, int bits, int nLSBfirst) {
-    spi_printf("[MOSI08][%02d] -> ", bits);
+extern int libswd_drv_mosi_8(libswd_ctx_t *libswdctx, libswd_cmd_t *cmd, char *data, int bits, int nLSBfirst)
+{
+    ESP_LOGD(TAG, "[MOSI08][%02d] -> ", bits);
 
-    spi_device_handle_t* devSPI = (spi_device_handle_t*)libswdctx->driver->device;
+    spi_device_handle_t *spi_dev = (spi_device_handle_t *) libswdctx->driver->device;
 
-    spi_transaction_t transSPI;
-    memset(&transSPI, 0, sizeof(spi_transaction_t));
+    spi_transaction_t spi_trans;
+    memset(&spi_trans, 0, sizeof(spi_transaction_t));
 
-    transSPI.flags = SPI_TRANS_USE_TXDATA;
-    transSPI.cmd = 0; // If want to set, change command_bits
-    transSPI.addr = 0; // If want to set, change address_bits
-    transSPI.length = bits; // Bits
-    transSPI.rxlength = 0;
-    transSPI.tx_data[0] = *data;
-
-    direction_set(DIRECTION_GPIO, DIRECTION_MOSI);
-    transmitSPI(*devSPI, &transSPI);
-    direction_set(DIRECTION_GPIO, DIRECTION_MISO);
-
-    spi_printf("%02x", transSPI.tx_data[0]);
-    
-    spi_printf(";\n");
-    return LIBSWD_OK;
-}
-
-extern int libswd_drv_mosi_32(libswd_ctx_t *libswdctx, libswd_cmd_t *cmd, int *data, int bits, int nLSBfirst) {
-    spi_printf("[MOSI32][%02d] -> ", bits);
-
-    spi_device_handle_t* devSPI = (spi_device_handle_t*)libswdctx->driver->device;
-
-    spi_transaction_t transSPI;
-    memset(&transSPI, 0, sizeof(spi_transaction_t));
-
-    transSPI.flags = SPI_TRANS_USE_TXDATA;
-    transSPI.cmd = 0; // If want to set, change command_bits
-    transSPI.addr = 0; // If want to set, change address_bits
-    transSPI.length = bits; // Bits
-    transSPI.rxlength = 0;
-
-    *((int*)(&transSPI.tx_data)) = (*data);
+    spi_trans.flags = SPI_TRANS_USE_TXDATA;
+    spi_trans.cmd = 0; // If want to set, change command_bits
+    spi_trans.addr = 0; // If want to set, change address_bits
+    spi_trans.length = bits; // Bits
+    spi_trans.rxlength = 0;
+    spi_trans.tx_data[0] = *data;
 
     direction_set(DIRECTION_GPIO, DIRECTION_MOSI);
-    transmitSPI(*devSPI, &transSPI);
+    spi_transmit(*spi_dev, &spi_trans);
     direction_set(DIRECTION_GPIO, DIRECTION_MISO);
 
-    spi_printf("%02x %02x %02x %02x", transSPI.tx_data[0], transSPI.tx_data[1], transSPI.tx_data[2], transSPI.tx_data[3]);
-    
-    spi_printf(";\n");
+    ESP_LOGD(TAG, "%02x", spi_trans.tx_data[0]);
+
+    ESP_LOGD(TAG, ";\n");
     return LIBSWD_OK;
 }
 
-extern int libswd_drv_miso_8(libswd_ctx_t *libswdctx, libswd_cmd_t *cmd, char *data, int bits, int nLSBfirst) {
-    spi_printf("[MISO08][%02d] <- ", bits);
+extern int libswd_drv_mosi_32(libswd_ctx_t *libswdctx, libswd_cmd_t *cmd, int *data, int bits, int nLSBfirst)
+{
+    ESP_LOGD(TAG, "[MOSI32][%02d] -> ", bits);
 
-    spi_device_handle_t* devSPI = (spi_device_handle_t*)libswdctx->driver->device;
+    spi_device_handle_t *spi_dev = (spi_device_handle_t *) libswdctx->driver->device;
 
-    spi_transaction_t transSPI;
-    memset(&transSPI, 0, sizeof(spi_transaction_t));
+    spi_transaction_t spi_trans;
+    memset(&spi_trans, 0, sizeof(spi_transaction_t));
 
-    transSPI.flags = SPI_TRANS_USE_RXDATA;
-    transSPI.cmd = 0; // If want to set, change command_bits
-    transSPI.addr = 0; // If want to set, change address_bits
-    transSPI.length = 0; // Bits
-    transSPI.rxlength = bits; // Bits expected
+    spi_trans.flags = SPI_TRANS_USE_TXDATA;
+    spi_trans.cmd = 0; // If want to set, change command_bits
+    spi_trans.addr = 0; // If want to set, change address_bits
+    spi_trans.length = bits; // Bits
+    spi_trans.rxlength = 0;
 
-    transmitSPI(*devSPI, &transSPI);
-
-    spi_printf("%02x", transSPI.rx_data[0]);
-
-    (*data) = transSPI.rx_data[0];
-
-    spi_printf(";\n");
-    return LIBSWD_OK;
-}
-
-extern int libswd_drv_miso_32(libswd_ctx_t *libswdctx, libswd_cmd_t *cmd, int *data, int bits, int nLSBfirst) {
-    spi_printf("[MISO32][%02d] <- ", bits);
-
-    spi_device_handle_t* devSPI = (spi_device_handle_t*)libswdctx->driver->device;
-
-    spi_transaction_t transSPI;
-    memset(&transSPI, 0, sizeof(spi_transaction_t));
-
-    transSPI.flags = SPI_TRANS_USE_RXDATA;
-    transSPI.cmd = 0; // If want to set, change command_bits
-    transSPI.addr = 0; // If want to set, change address_bits
-    transSPI.length = 0; // Bits
-    transSPI.rxlength = bits; // Bits expected
-
-    transmitSPI(*devSPI, &transSPI);
-
-    spi_printf("%02x %02x %02x %02x", transSPI.rx_data[0], transSPI.rx_data[1], transSPI.rx_data[2], transSPI.rx_data[3]);
-
-    (*data) = *((int*)(&transSPI.rx_data));
-
-    spi_printf(";\n");
-    return LIBSWD_OK;
-}
-
-extern int libswd_drv_mosi_trn(libswd_ctx_t *libswdctx, int clks) {
-    spi_printf("[MOSITN][%02d]", clks);
-
-    if(clks == 0) { return LIBSWD_OK; }
-
-    spi_device_handle_t* devSPI = (spi_device_handle_t*)libswdctx->driver->device;
-
-    spi_transaction_t transSPI;
-    memset(&transSPI, 0, sizeof(spi_transaction_t));
-
-    transSPI.flags = SPI_TRANS_USE_TXDATA;
-    transSPI.cmd = 0; // If want to set, change command_bits
-    transSPI.addr = 0; // If want to set, change address_bits
-    transSPI.length = clks; // Bits
-    transSPI.rxlength = 0;
+    *((int *) (&spi_trans.tx_data)) = (*data);
 
     direction_set(DIRECTION_GPIO, DIRECTION_MOSI);
-    transmitSPI(*devSPI, &transSPI);
+    spi_transmit(*spi_dev, &spi_trans);
     direction_set(DIRECTION_GPIO, DIRECTION_MISO);
 
-    spi_printf(";\n");
+    ESP_LOGD(TAG, "%02x %02x %02x %02x", spi_trans.tx_data[0], spi_trans.tx_data[1], spi_trans.tx_data[2],
+             spi_trans.tx_data[3]);
+
+    ESP_LOGD(TAG, ";\n");
     return LIBSWD_OK;
 }
 
-extern int libswd_drv_miso_trn(libswd_ctx_t *libswdctx, int clks) {
-    spi_printf("[MISOTN][%02d]", clks);
+extern int libswd_drv_miso_8(libswd_ctx_t *libswdctx, libswd_cmd_t *cmd, char *data, int bits, int nLSBfirst)
+{
+    ESP_LOGD(TAG, "[MISO08][%02d] <- ", bits);
 
-    if(clks == 0) { return LIBSWD_OK; }
-    spi_device_handle_t* devSPI = (spi_device_handle_t*)libswdctx->driver->device;
+    spi_device_handle_t *spi_dev = (spi_device_handle_t *) libswdctx->driver->device;
 
-    spi_transaction_t transSPI;
-    memset(&transSPI, 0, sizeof(spi_transaction_t));
+    spi_transaction_t spi_trans;
+    memset(&spi_trans, 0, sizeof(spi_transaction_t));
 
-    transSPI.flags = SPI_TRANS_USE_TXDATA;
-    transSPI.cmd = 0; // If want to set, change command_bits
-    transSPI.addr = 0; // If want to set, change address_bits
-    transSPI.length = clks;
-    transSPI.rxlength = 0;
+    spi_trans.flags = SPI_TRANS_USE_RXDATA;
+    spi_trans.cmd = 0; // If want to set, change command_bits
+    spi_trans.addr = 0; // If want to set, change address_bits
+    spi_trans.length = 0; // Bits
+    spi_trans.rxlength = bits; // Bits expected
+
+    spi_transmit(*spi_dev, &spi_trans);
+
+    ESP_LOGD(TAG, "%02x", spi_trans.rx_data[0]);
+
+    (*data) = spi_trans.rx_data[0];
+
+    ESP_LOGD(TAG, ";\n");
+    return LIBSWD_OK;
+}
+
+extern int libswd_drv_miso_32(libswd_ctx_t *libswdctx, libswd_cmd_t *cmd, int *data, int bits, int nLSBfirst)
+{
+    ESP_LOGD(TAG, "[MISO32][%02d] <- ", bits);
+
+    spi_device_handle_t *spi_dev = (spi_device_handle_t *) libswdctx->driver->device;
+
+    spi_transaction_t spi_trans;
+    memset(&spi_trans, 0, sizeof(spi_transaction_t));
+
+    spi_trans.flags = SPI_TRANS_USE_RXDATA;
+    spi_trans.cmd = 0; // If want to set, change command_bits
+    spi_trans.addr = 0; // If want to set, change address_bits
+    spi_trans.length = 0; // Bits
+    spi_trans.rxlength = bits; // Bits expected
+
+    spi_transmit(*spi_dev, &spi_trans);
+
+
+    (*data) = *((int *) (&spi_trans.rx_data));
+
+    return LIBSWD_OK;
+}
+
+extern int libswd_drv_mosi_trn(libswd_ctx_t *libswdctx, int clks)
+{
+    ESP_LOGD(TAG, "[MOSITN][%02d]", clks);
+
+    if (clks == 0) { return LIBSWD_OK; }
+
+    spi_device_handle_t *spi_dev = (spi_device_handle_t *) libswdctx->driver->device;
+
+    spi_transaction_t spi_trans;
+    memset(&spi_trans, 0, sizeof(spi_transaction_t));
+
+    spi_trans.flags = SPI_TRANS_USE_TXDATA;
+    spi_trans.cmd = 0; // If want to set, change command_bits
+    spi_trans.addr = 0; // If want to set, change address_bits
+    spi_trans.length = clks; // Bits
+    spi_trans.rxlength = 0;
 
     direction_set(DIRECTION_GPIO, DIRECTION_MOSI);
-    transmitSPI(*devSPI, &transSPI);
+    spi_transmit(*spi_dev, &spi_trans);
     direction_set(DIRECTION_GPIO, DIRECTION_MISO);
 
-    spi_printf(";\n");
+    ESP_LOGD(TAG, ";\n");
+    return LIBSWD_OK;
+}
+
+extern int libswd_drv_miso_trn(libswd_ctx_t *libswdctx, int clks)
+{
+    ESP_LOGD(TAG, "[MISOTN][%02d]", clks);
+
+    if (clks == 0) { return LIBSWD_OK; }
+    spi_device_handle_t *spi_dev = (spi_device_handle_t *) libswdctx->driver->device;
+
+    spi_transaction_t spi_trans;
+    memset(&spi_trans, 0, sizeof(spi_transaction_t));
+
+    spi_trans.flags = SPI_TRANS_USE_TXDATA;
+    spi_trans.cmd = 0; // If want to set, change command_bits
+    spi_trans.addr = 0; // If want to set, change address_bits
+    spi_trans.length = clks;
+    spi_trans.rxlength = 0;
+
+    direction_set(DIRECTION_GPIO, DIRECTION_MOSI);
+    spi_transmit(*spi_dev, &spi_trans);
+    direction_set(DIRECTION_GPIO, DIRECTION_MISO);
+
+    ESP_LOGD(TAG, ";\n");
 
     return LIBSWD_OK;
 }
 
-#include "esp_log.h"
-#define TAG "libswd"
-extern int libswd_log(libswd_ctx_t *libswdctx, libswd_loglevel_t loglevel, char *msg, ...) {
+extern int libswd_log(libswd_ctx_t *libswdctx, libswd_loglevel_t loglevel, char *msg, ...)
+{
     va_list args;
     va_start (args, msg);
 
     char buffer[256];
     int length = 0;
-    length = vsnprintf (buffer, 255, msg, args);
+    length = vsnprintf(buffer, 255, msg, args);
     buffer[length] = '\0';
 
     switch (loglevel) {
@@ -255,6 +261,7 @@ extern int libswd_log(libswd_ctx_t *libswdctx, libswd_loglevel_t loglevel, char 
     return LIBSWD_OK;
 }
 
-extern int libswd_log_level_inherit(libswd_ctx_t *libswdctx, int loglevel) {
+extern int libswd_log_level_inherit(libswd_ctx_t *libswdctx, int loglevel)
+{
     return LIBSWD_OK;
 }
